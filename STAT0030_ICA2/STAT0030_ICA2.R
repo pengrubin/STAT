c1d8b3484c51cd52b201f6f8f1f4e4edd3e39d04
pengrubin <pengrubin@foxmail.com>
2019-02-18 00:58:20 +0000

    Update STAT0030_ICA2.R

--- STAT0030_ICA2/STAT0030_ICA2.R
+++ STAT0030_ICA2/STAT0030_ICA2.R
@@ -17,23 +17,27 @@ pairs(logdata[,2:4], # plot log(hp), log(wt), log(mpg)
       main = "Plot Between log Variables -- 3 species", #add the main title
       pch = c(21,24)[unclass(logdata$tr)+1], #different tr shows different shape
       bg = c("red", "green3")[unclass(logdata$tr)+1]) #different tr shows different colour
-
 boxplot(mpg~tr, #MPG by TR
         data=rawdata, #set the dataset
         xlab="Gear Transmission Type" , #add the xlab title
         ylab="Miles Per Gallon", #add the ylab title
         main="MPG by Gear Transmission Type", #add the main title
         names=c("Automatic","Manual")) #change xlab value to character
-t.test(mpg~tr, data=logdata)
+t.test(mpg~tr, data=logdata)# the t-test above MPG is related to TR.
+
+#question_1_c
 model<-lm(mpg~tr+hp+wt, data=logdata); # i.e, full without qsec and gears
 summary(model)
 summary(model$residuals)
 par(mfrow=c(2,2)) #put 4 graphes together
-plot(model) #plot 4 graphes as following
-
+plot(model) #plot 4 graphes as following. 
+#ddition: if you run the last line code directly, there will be an error "figure margins too large".
+#there two way to fix it: 1, click "clear all plots" and then run it. 
+#                         2, run >>x11() at first if you are run in Windows computer.
+#BTW: there is no error if i use it in R Markdown
 best<-step(model, direction="both")
 summary(best)
 summary(best$residuals)
 par(mfrow=c(2,2)) #put 4 graphes together
-plot(best)#plot 4 graphes as following
+plot(best)#there will be the same error as line 33 and line 34-36 is the solvtion.
 
