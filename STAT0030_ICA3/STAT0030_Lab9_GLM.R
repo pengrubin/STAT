55dee2d35803c3a7ec5b8dfb30d0a815b9ec308e
pengrubin <pengrubin@foxmail.com>
2019-04-23 02:42:39 +0100

    merge

--- STAT0030_ICA3/STAT0030_Lab9_GLM.R
+++ STAT0030_ICA3/STAT0030_Lab9_GLM.R
@@ -1,3 +1,7 @@
 #Use read.table to read the data into R
 storm.data <- read.table("/Users/hongwei/Documents/GitHub/STAT/STAT0030_ICA3/STAT0030_Lab8&9_GLM/nstorms.dat",header = T)
 nino.data <- read.table("/Users/hongwei/Documents/GitHub/STAT/STAT0030_ICA3/STAT0030_Lab8&9_GLM/nino3.dat",header = T)
+
+#By adding 1 to the years in nino.data frame before merging, we match the storm data.
+nino.data$Year <- nino.data$Year + 1
+storm.data <- merge(storm.data,nino.data)
