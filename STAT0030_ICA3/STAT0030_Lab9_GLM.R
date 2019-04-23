41ae1c96078f0eb415acd3d3cf107a427692a8e7
pengrubin <pengrubin@foxmail.com>
2019-04-23 02:39:35 +0100

    #Use read.table to read the data into R

--- STAT0030_ICA3/STAT0030_Lab9_GLM.R
+++ STAT0030_ICA3/STAT0030_Lab9_GLM.R
@@ -1,1 +1,3 @@
+#Use read.table to read the data into R
+storm.data <- read.table("/Users/hongwei/Documents/GitHub/STAT/STAT0030_ICA3/STAT0030_Lab8&9_GLM/nstorms.dat",header = T)
 nino.data <- read.table("/Users/hongwei/Documents/GitHub/STAT/STAT0030_ICA3/STAT0030_Lab8&9_GLM/nino3.dat",header = T)
