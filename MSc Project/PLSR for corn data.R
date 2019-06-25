f8a0aad56542f7b665a042a059a0d551d500a753
pengrubin <pengrubin@foxmail.com>
2019-06-25 04:14:38 +0100

    Update PLSR for corn data.R

--- MSc Project/PLSR for corn data.R
+++ MSc Project/PLSR for corn data.R
@@ -11,38 +11,35 @@ n=60                             #the number of calibration
 sample <- sample(1:80)           #set random order; the begin of reset order
 calibration <- cbind(propvals[,1][sample[1:n] ],m5data[sample[1:n], ])#first nth rows for calibration
 prediction <- cbind(propvals[,1][sample[n+1:80] ],m5data[sample[n+1:80], ])#n+1th to the end sample for  prediction
-
-corn.pls <- plsr(calibration[,1]~calibration[,-1], ncomp = 10,data= calibration, validation="LOO",jackknife=TRUE,method="widekernelpls")
+corn.pls <- plsr(calibration[,1]~calibration[,-1], ncomp = 10, validation="LOO",jackknife=TRUE,method="widekernelpls")
 summary(corn.pls,what="all")
-pred.resp <- predict(corn.pls, ncomp = 1:10,newdata = m5data[prediction])
+pred.resp <- predict(corn.pls, comps = 1:10, newdata = prediction[,-1])
 
 
 
 
-## 偏最小二乘回归
-library(lars)
-library(pls)
-
-ap = plsr(propvals[,1]~m5data, 10, validation = "CV")
-# CV准则下，不同评价指标
-validationplot(ap)
-# 不同准则
-RMSEP(ap)
-MSEP(ap)
-R2(ap)
 
 
 
-# NOT RUN {
-data(yarn)
-nir.mvr <- mvr(density ~ NIR, ncomp = 5, data = yarn[yarn$train,])
 
-## Predicted responses for models with 1, 2, 3 and 4 components
-pred.resp <- predict(nir.mvr, ncomp = 1:4, newdata = yarn[!yarn$train,])
+library("pls")
+set.seed(123)
+bands=101
+spectra = matrix(runif(56*bands),ncol=bands)
+DF <- data.frame(spectra = I(spectra),
+                 height = rpois(56,10),
+                 fbm = rpois(56,10),
+                 nitrogen = rpois(56,10),
+                 carbon = rpois(56,10),
+                 chl = rpois(56,10),
+                 ID = 1:56)
+class(DF$spectra) <- "matrix" # just to be certain, it was "AsIs"
+str(DF)
 
-## Predicted responses for a single model with components 1, 2, 3, 4
-predict(nir.mvr, comps = 1:4, newdata = yarn[!yarn$train,])
+DF$train <- rep(FALSE, 56)
+DF$train[1:20] <- TRUE
 
-## Predicted scores
-predict(nir.mvr, comps = 1:3, type = "scores", newdata = yarn[!yarn$train,])
-# }
+refl.pls <- plsr(height ~ spectra, data = DF, ncomp = 10, validation = 
+                   "LOO", jackknife = TRUE, subset = train)
+
+res <- predict(refl.pls, ncomp = 3, newdata = DF[!DF$train,])
