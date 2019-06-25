e7a3c7b8c41a2fa2d4301d8d04c7bb32274af223
pengrubin <pengrubin@foxmail.com>
2019-06-25 03:34:09 +0100

    Update PLSR for corn data.R

--- MSc Project/PLSR for corn data.R
+++ MSc Project/PLSR for corn data.R
@@ -1,15 +1,23 @@
-library("R.matlab")
+library(R.matlab)
 library(pls) 
+library(lars)
 rawdata <- readMat("/Users/hongwei/Downloads/corn.mat")
 m5data <- rawdata$m5spec$data
 mp5data <- rawdata$mp5spec$data
+mp6data <- rawdata$mp6spec$data
 propvals <- rawdata$propvals$data
 
-corn.pls <- plsr(m5data, propvals[,1], ncomp = 4, mode = "regression", tol = 1e-25)
+n=60                             #the number of calibration
+sample <- sample(1:80)           #set random order; the begin of reset order
+calibration <- cbind(propvals[,1][sample[1:n] ],m5data[sample[1:n], ])#first nth rows for calibration
+prediction <- cbind(propvals[,1][sample[n+1:80] ],m5data[sample[n+1:80], ])#n+1th to the end sample for  prediction
+
+corn.pls <- plsr(calibration[,1]~calibration[,-1], ncomp = 10,data= calibration, validation="LOO",jackknife=TRUE,method="widekernelpls")
+summary(corn.pls,what="all")
+pred.resp <- predict(corn.pls, ncomp = 1:10,newdata = m5data[prediction])
 
 
-pls1<-plsr(propvals[,1]~m5data,10,validation="LOO",jackknife=TRUE,method="widekernelpls")
-summary(pls1,what="all")
+
 
 ## 偏最小二乘回归
 library(lars)
