9b74775393a3a81f0ca129c238e29240342ca9c0
pengrubin <pengrubin@foxmail.com>
2019-07-04 08:48:50 +0100

    finished

--- MSc Project/scaled PLS for corn data test.R
+++ MSc Project/scaled PLS for corn data test.R
@@ -12,7 +12,7 @@ corn_PLS=function(n){                                #n is the number of calibra
   NV <- 10                                           #number of variables 
   sample <- sample(1:80)                             #set random order; the begin of reset order
   DF <- data.frame(NIR = I(m5data),                  #input data
-                   y=propvals[,1])
+                   y=propvals[,2])
   class(DF$NIR) <- "matrix"                          # just to be certain, it was "AsIs"
   #str(DF)                                           #check point
   DF$train <- rep(FALSE, 80)
@@ -28,12 +28,12 @@ corn_PLS=function(n){                                #n is the number of calibra
   return(cbind(RMSECV,RMSEP))                        #return 1x2matrix
 }
 #corn_PLS(n)
-n <- as.matrix(rep(20:70,10))                        #the number of calibration, rep(a:b,c): from a to b and repeat c. 
+n <- as.matrix(rep(40,50))                        #the number of calibration, rep(a:b,c): from a to b and repeat c. 
 PlsResult <- apply(n,1,corn_PLS)                     #loop
 PlotData <- as.data.frame(cbind(n,t(PlsResult)))     #combind the results
 par(mfrow=c(1,2))
 boxplot(V2~V1,data=PlotData,xlab="Number of Calibration", ylab="RMSECV",main="PLS after scaled")
 boxplot(V3~V1,data=PlotData,xlab="Number of Calibration", ylab="RMSEP",main="PLS after scaled")
-apply(PlotData,2,mean)
-
-
+PlotDataMean <- apply(PlotData,2,mean)
+PlotDataSd <- apply(PlotData,2,sd)
+cat(PlotDataMean[3]-PlotDataSd[3],PlotDataMean[3],PlotDataMean[3]+PlotDataSd[3])
